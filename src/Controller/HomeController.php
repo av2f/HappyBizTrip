bdc63efe5cc77a1d6b825dab64c956b8bf77bbdf
unknown <fparmentier@free.fr>
2020-04-21 08:41:08 +0200

    homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(Request $request)
+    {
+        $locale = $request->getLocale();
+        
+        return $this->render('home/index.html.twig', [
+            'locale' => $locale,
+        ]);
+    }
+}
