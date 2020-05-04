aea9c5cb4df94f21d997f269e850898265e7c917
unknown <fparmentier@free.fr>
2020-05-04 09:02:47 +0200

    build registration form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\HomeRegisterType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,13 +12,23 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
+     * 
      */
     public function index(Request $request)
     {
         $locale = $request->getLocale();
+
+        $user = new User;
+        
+        // create form
+        $form = $this->createForm(HomeRegisterType::class, $user);
+        
+        // handle the submit
+        $form->handleRequest($request);
         
         return $this->render('home/index.html.twig', [
             'locale' => $locale,
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/HomeRegisterType.php
+++ src/Form/HomeRegisterType.php
@@ -6,16 +6,36 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class HomeRegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('pseudo')
-            ->add('email')
-            ->add('birthDate')
-            ->add('password')
+            ->add('pseudo', TextType::class, [
+                'label' => 'form.label_pseudo',
+                'help' => 'form.help_pseudo',
+                'attr' => [
+                    'placeholder' => 'form.placeholder_pseudo'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'form.label_email',
+                'attr' => [
+                    'placeholder' => 'form.placeholder_email'
+                ]
+            ])
+            // ->add('birthDate')
+            ->add('password', PasswordType::class, [
+                'label' => 'form.label_password',
+                'help' => 'form.help_password',
+                'attr' => [
+                    'placeholder' => 'form.placeholder_password'
+                ]
+            ])
         ;
     }
 
