c304e5298ee33d0035b83ed316d3255c17cd263c
Parmentier <fparmentier@free.fr>
2020-07-23 18:19:53 +0200

    work on buttons for list of profile

--- assets/js/listProfile.js
+++ assets/js/listProfile.js
@@ -19,28 +19,36 @@ document.querySelectorAll('.btn-connect').forEach(connect =>
         'X-Requested-With': 'XMLHttpRequest',
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({"_token": connect.dataset.token})
+      body: JSON.stringify({'_token': connect.dataset.token})
     })
     .then(
       response => response.json()
     )
     .then(data => {
       if (data.success) {
-        console.log("youpi ça a marché")
+        console.log('youpi ça a marché')
         // remove button
         connect.parentElement.remove()
         // display badge
-        var btnId = connect.getAttribute("id")
-        var badgeId = "B_" + btnId.substring(btnId.indexOf('_',1) + 1, btnId.length)
-        if (document.getElementById(badgeId).style.display == "none") {
-            document.getElementById(badgeId).style.display = "inline"
+        var btnId = connect.getAttribute('id')
+        var badgeId = 'B_' + btnId.substring(btnId.indexOf('_', 1) + 1, btnId.length)
+        if (document.getElementById(badgeId).style.display === 'none') {
+            document.getElementById(badgeId).style.display = 'inline'
         }
         console.log(document.getElementById(badgeId).innerHTML)
       }
       else {
-        console.log("merde ça a foiré")
+        console.log('merde ça a foiré')
       }
     })
     .catch(e => alert(e))
+  })
+)
+
+// Manage Cancel button
+document.querySelectorAll('.btn-cancel').forEach(cancel => 
+  cancel.addEventListener('click', (e) => { 
+    var btnId = cancel.getAttribute('id')
+    console.log(btnId)
   })
 )
